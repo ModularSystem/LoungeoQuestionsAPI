5eab6f8c3dd3fcdabd6ce9d7535f49e1c2cef03f
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-11-18 14:26:36 -0800

    re-wrote data load to update sequence after import

--- server/db/loadDB.sql
+++ server/db/loadDB.sql
@@ -4,6 +4,9 @@
 \COPY answer_photos FROM './csv/answers_photosCSV.csv' WITH DELIMITER ',' CSV HEADER;
 
 
+SELECT setval(pg_get_serial_sequence('questions', 'question_id'), (SELECT MAX(question_id) from "questions"));
+SELECT setval(pg_get_serial_sequence('answers', 'answer_id'), (SELECT MAX(answer_id) from "answers"));
+SELECT setval(pg_get_serial_sequence('answer_photos', 'photo_id'), (SELECT MAX(answer_id) from "answer_photos"));
 
 -- INSERT INTO users(name, email) SELECT DISTINCT name, email FROM questions ;
 
