ccd338d5f6b7275381db627c1776738a9927fe31
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-11-23 09:45:19 -0800

    deleted useless function call from server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 
 const port = 3000;
-const postgresConfig = require('./db/poolConfig');
+// const postgresConfig = require('./db/poolConfig');
 
 if (require.main === module) {
   app.listen(port, () => {
     console.log(`Questions and Answers API running on ${port}`);
   });
 }
-postgresConfig();
+
