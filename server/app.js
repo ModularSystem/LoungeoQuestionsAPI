ec7dc954de9e9930e1fec4643499d6d3b67d70a3
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-11-29 13:38:04 -0800

    removed console log

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 const { question, answer } = require('./models');
 
 app.get('/loaderio-0f17028868e34d7bd1b89796b40b85c3.txt', (req, res) => {
-  console.log(__dirname)
-  res.sendFile('/public/loaderio-0f17028868e34d7bd1b89796b40b85c3.txt')
+
+  res.sendFile('../public/loaderio-0f17028868e34d7bd1b89796b40b85c3.txt')
 })
 
 app.get('/qa/questions', async (req, res) => {
