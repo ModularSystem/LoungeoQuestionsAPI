1d1bc3196fd5b045777f2b8f88ea84ada3b803ea
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-01 13:27:19 -0800

    un-commented model code

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,30 +1,30 @@
 const pool = require('../db/poolConfig');
 
 module.exports = {
-  ///// PROMISES
-  select: async (params) => {
-    const { questionID, count, offset } = params;
-    const result = await pool.query(`SELECT * FROM answers WHERE question_id = ${questionID} LIMIT ${count} OFFSET ${offset}`);
-    return Promise.all(result.rows.map(async (answer) => {
-      const photos = await pool.query(
-        `SELECT * FROM answer_photos WHERE answer_id = ${answer.id}`,
-      );
-      answer.photos = photos.rows;
-      return answer;
-    }));
-  },
-  /// JSON AGG
-  // select: (params) => {
+  // ///// PROMISES
+  // select: async (params) => {
   //   const { questionID, count, offset } = params;
-  //   return pool.query(`SELECT *,  (
-  //     SELECT json_agg(photo) FROM (
-  //       SELECT *
-
-  //        FROM answer_photos WHERE answers.id = answer_photos.photo_id
-  //      ) as photo
-  // ) as photos
-  //   FROM answers WHERE question_id = ${questionID} LIMIT ${count} OFFSET ${offset}`);
+  //   const result = await pool.query(`SELECT * FROM answers WHERE question_id = ${questionID} LIMIT ${count} OFFSET ${offset}`);
+  //   return Promise.all(result.rows.map(async (answer) => {
+  //     const photos = await pool.query(
+  //       `SELECT * FROM answer_photos WHERE answer_id = ${answer.id}`,
+  //     );
+  //     answer.photos = photos.rows;
+  //     return answer;
+  //   }));
   // },
+  /// JSON AGG
+  select: (params) => {
+    const { questionID, count, offset } = params;
+    return pool.query(`SELECT *,  (
+      SELECT json_agg(photo) FROM (
+        SELECT *
+
+         FROM answer_photos WHERE answers.id = answer_photos.photo_id
+       ) as photo
+  ) as photos
+    FROM answers WHERE question_id = ${questionID} LIMIT ${count} OFFSET ${offset}`);
+  },
 
   insert: (params) => {
     const {
